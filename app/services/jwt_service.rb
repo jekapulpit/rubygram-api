cb4f789839596904241eef8f9932f4e493383a08
Yauhen <q010@bk.ru>
2019-10-01 19:07:08 +0300

    added secret key into env

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -2,11 +2,11 @@
 
 class JwtService
   def self.encode(payload)
-    JWT.encode(payload, 'fe3c8afbcd459a95aa00f50aeb3a4c117ee057af2134734caf76e51d5bae9eee20ab5c9f9557aebf156864c389ba6f8f229dade7345dac1363980f0920164092', 'HS256')
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'], 'HS256')
   end
 
   def self.decode(token)
-    body, = JWT.decode(token, Rails.application.secrets.secret_key_base,
+    body, = JWT.decode(token, ENV['SECRET_KEY_BASE'],
                        true, algorithm: 'HS256')
     HashWithIndifferentAccess.new(body)
   rescue JWT::ExpiredSignature
