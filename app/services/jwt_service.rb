498f309520c696a8daec3be42cf9c051c5cf6cb9
Yauhen <q010@bk.ru>
2019-10-01 19:03:18 +0300

    added secret key into env

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -2,7 +2,7 @@
 
 class JwtService
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+    JWT.encode(payload, 'fe3c8afbcd459a95aa00f50aeb3a4c117ee057af2134734caf76e51d5bae9eee20ab5c9f9557aebf156864c389ba6f8f229dade7345dac1363980f0920164092', 'HS256')
   end
 
   def self.decode(token)
