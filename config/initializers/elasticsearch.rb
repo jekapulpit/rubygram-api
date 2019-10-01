0fb83b8caa81a7fd308bd6947729f0e737485dbf
Yauhen <q010@bk.ru>
2019-10-01 18:11:12 +0300

    es install

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,11 +1,11 @@
 if ENV.include?("PROD")
-  Elasticsearch::Model.client = Elasticsearch::Client.new hosts: [{
-                                                                      host: ENV["FOUNDELASTICSEARCH_URL"].split("https://")[1],
-                                                                      user: ENV["ES_USER"],
-                                                                      port: "9243",
-                                                                      password: ENV["ES_PW"],
-                                                                      scheme: "https"
-                                                                  }]
+  Searchkick.client = Elasticsearch::Client.new hosts: [{
+                                                            host: ENV["FOUNDELASTICSEARCH_URL"].split("https://")[1],
+                                                            user: ENV["ES_USER"],
+                                                            port: "9243",
+                                                            password: ENV["ES_PW"],
+                                                            scheme: "https"
+                                                        }]
 else
-  Elasticsearch::Model.client = Elasticsearch::Client.new host: "http://localhost:9200"
+  Searchkick.client = Elasticsearch::Client.new host: "http://localhost:9200"
 end
