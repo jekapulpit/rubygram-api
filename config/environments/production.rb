e9ecaa92b60272d52dde5a18267c2dc6e07da103
Yauhen <q010@bk.ru>
2019-10-01 19:14:03 +0300

    added rq origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
-
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
