a49adcd87c3557d17987d8c80d8347d88bad08a6
Yauhen <q010@bk.ru>
2019-10-04 12:43:37 +0300

    changed ES engine

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,11 +1,5 @@
 if ENV.include?("PROD")
-  Searchkick.client = Elasticsearch::Client.new hosts: [{
-                                                            host: ENV["FOUNDELASTICSEARCH_URL"].split("https://")[1],
-                                                            user: ENV["ES_USER"],
-                                                            port: "9243",
-                                                            password: ENV["ES_PW"],
-                                                            scheme: "https"
-                                                        }]
+  Searchkick.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
 else
   Searchkick.client = Elasticsearch::Client.new host: "http://localhost:9200"
 end
