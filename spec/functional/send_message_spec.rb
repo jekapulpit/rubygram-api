0c4200635812704f858d5289a69eb1782a94b400
Yauhen <q010@bk.ru>
2019-10-04 14:34:24 +0300

    separated browser operations into page classes

--- spec/functional/send_message_spec.rb
+++ spec/functional/send_message_spec.rb
@@ -2,28 +2,29 @@
 
 require 'rails_helper'
 
-RSpec.describe 'send message' do
+RSpec.describe 'send message', type: :feature do
   let(:room) { Room.first }
   let(:sender) { room.users.first }
   let(:receiver) { room.users.last }
   let(:test_message_content) { 'test_message' }
 
   context 'from senders view' do
-    let(:browser) { Support::SignUp.new.call({:email => sender.email, :password => '123456'}) }
+    before do
+      Support::SignUp.new(@browser).call({:email => sender.email, :password => '123456'})
+      chat_page = ChatPage.new(room.id, @browser)
+      chat_page.send_message(test_message_content)
+    end
     it 'message should be sent' do
-      browser.a(:href, '/home/rooms').click
-      browser.a(:href, "/home/rooms/#{room.id}").click
-      browser.text_field(:id, 'message-field').set(test_message_content)
-      browser.button(:id, 'send-message-btn').click
       browser.text.should match(/.*#{test_message_content}.*/)
     end
   end
 
   context 'from receivers view' do
-    let(:browser) { Support::SignUp.new.call({:email => sender.email, :password => '123456'}) }
+    before do
+      Support::SignUp.new(@browser).call({:email => sender.email, :password => '123456'})
+      chat_page = ChatPage.new(room.id, @browser)
+    end
     it 'message should be received' do
-      browser.a(:href, '/home/rooms').click
-      browser.a(:href, "/home/rooms/#{room.id}").click
       browser.text.should match(/.*#{test_message_content}.*/)
     end
   end
